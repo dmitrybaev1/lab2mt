59bff434135f5d2d5c8f6e5137e4a3ca75fd7a88
dmitrybaev1 <223868@niuitmo.ru>
2020-02-10 14:46:30 +0300

    сделан getopt, все еще один ключ за вызов

--- lab2Multithreading/main.c
+++ lab2Multithreading/main.c
@@ -4,19 +4,57 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
-int number=0;
-int shownonprinting=0;
-int showends=0;
-int showtabs=0;
-int squeezeblank=0;
+typedef struct globalArgs_t{
+    int number;// -n
+    int shownonprinting;// -v
+    int showends;// -E
+    int showtabs;// -T
+    int squeezeblank;// -s
+    char **files;
+    int numfiles;
+}globalArgs;
+globalArgs args={0,0,0,0,0,NULL,0};
+const char *optstring="nvETs";
 char src[256];
 int squeezeflag=0;
 int numberflag=1;
 void readfile(char* filename);
-void print(char c,int *i);
+void print(unsigned char c,int *i);
 //int checkstdi(char *arg);
 int main(int argc, char *argv[]) {
+    int opt=0;
     if(argc>1) {
+        opt = getopt(argc, argv, optstring);
+        while (opt != -1) {
+            switch (opt) {
+                case 'n':
+                    args.number = 1;
+                    break;
+                case 'v':
+                    args.shownonprinting = 1;
+                    break;
+                case 'E':
+                    args.showends = 1;
+                    break;
+                case 'T':
+                    args.showtabs = 1;
+                    break;
+                case 's':
+                    args.squeezeblank = 1;
+                    break;
+                default:
+                    break;
+            }
+            opt = getopt(argc, argv, optstring);
+        }
+        args.files = argv + optind;
+        args.numfiles = argc - optind;
+        for (int i = 0; i < args.numfiles; i++) {
+            readfile(args.files[i]);
+        }
+    } else
+        readfile("-");
+   /* if(argc>1) {
         for (int i = 1; i < argc; i++) {//ПЕРЕБОР ПАРАМЕТРОВ
             if(i==1&&argc!=2) {
                 if (strlen(argv[i]) == 2) {//ВОЗМОЖНО, ЗДЕСЬ КЛЮЧИ
@@ -44,21 +82,11 @@ int main(int argc, char *argv[]) {
     }
     else{
         readfile("-");
-    }
+    }*/
     return 0;
 }
-/*int checkstdi(char *arg){
-    if (strlen(arg) == 1)
-        if (strcmp(arg,"-")==0) {
-            return 1;
-        } else {
-            return 0;
-        }
-    else
-        return 0;
-}*/
 
-void printnonprinting(char c){
+void printnonprinting(unsigned char c){
     if(c>=0&&c<=37&&c!=9&&c!=10&&c!=12) {
         c = c+64;
         printf("%c", '^');
@@ -69,26 +97,26 @@ void printnonprinting(char c){
     else
         printf("%c",c);
 }
-void printwithtabs(char c){
+void printwithtabs(unsigned char c){
     if(c==9)
         printf("%s","^I");
     else
         printf("%c",c);
 }
-void printwithends(char c){
+void printwithends(unsigned char c){
     if(c==10)
         printf("%s\n","$");
     else
         printf("%c",c);
 }
-void printwithnumberfile(char c, int *i){
+void printwithnumberfile(unsigned char c, int *i){
     if (c == 10) {
         printf("\n%6d  ", *i);
         (*i)++;
     } else
         printf("%c", c);
 }
-void printwithnumberstdin(char c,int *i){
+void printwithnumberstdin(unsigned char c,int *i){
     if(numberflag){
         numberflag=0;
         printf("%6d  ", *i);
@@ -97,7 +125,7 @@ void printwithnumberstdin(char c,int *i){
     } else
         printf("%c", c);
 }
-void printsqueezeblank(char c){
+void printsqueezeblank(unsigned char c){
     if(squeezeflag > 1) {
         if (c != 10){
             printf("%c",c);
@@ -112,16 +140,16 @@ void printsqueezeblank(char c){
         printf("%c",c);
     }
 }
-void print(char c, int *i){
-    if(shownonprinting)
+void print(unsigned char c, int *i){
+    if(args.shownonprinting)
         printnonprinting(c);
-    else if(showtabs)
+    else if(args.showtabs)
         printwithtabs(c);
-    else if(showends)
+    else if(args.showends)
         printwithends(c);
-    else if(number)
+    else if(args.number)
         printwithnumberfile(c, i);
-    else if(squeezeblank)
+    else if(args.squeezeblank)
         printsqueezeblank(c);
     else
         printf("%c",c);
@@ -131,9 +159,9 @@ void readfile(char* filename){
     int i=2;
     int j=1;
     int handle;
-    char ch;
+    unsigned char ch;
     if(strcmp(filename,"-")==0){//stdin
-        char c;
+        unsigned char c;
         int n;
         int count=0;
         while ((n = read(STDIN_FILENO, &c, 1)) >= 0) {
@@ -141,19 +169,19 @@ void readfile(char* filename){
             if (n == 0) {
                 break;
             } else {
-                if (shownonprinting)
+                if (args.shownonprinting)
                     printnonprinting(c);
-                else if (showtabs)
+                else if (args.showtabs)
                     printwithtabs(c);
-                else if (showends)
+                else if (args.showends)
                     printwithends(c);
-                else if (number) {
+                else if (args.number) {
                     printwithnumberstdin(c, &j);
                     if(c==10){
                         numberflag=1;
                     }
                 }
-                else if (squeezeblank)
+                else if (args.squeezeblank)
                     printsqueezeblank(c);
                 else
                     printf("%c", c);
@@ -167,7 +195,7 @@ void readfile(char* filename){
             perror(src);
             exit(1);
         }
-        if (number)
+        if (args.number)
             printf("\n     1  ");
         while (!eof(handle)) {
             if (read(handle, &ch, 1) == -1) {
