09096a1a40a1b627fb66aba705d2f33e544bf122
dmitrybaev1 <223868@niuitmo.ru>
2020-02-13 00:36:31 +0300

    поддерживаются все ключи одновременно(только file, stdin не готов), подравнять код и сделать stdin

--- lab2Multithreading/main.c
+++ lab2Multithreading/main.c
@@ -12,16 +12,30 @@ typedef struct globalArgs_t{
     int squeezeblank;// -s
     char **files;
     int numfiles;
-}globalArgs;
-globalArgs args={0,0,0,0,0,NULL,0};
+}globalargs;
+typedef struct flags_t{
+    int numberflag;
+    int nonprintingflag;
+    int endsflag;
+    int tabsflag;
+    int squeezeblankflag;
+}printflags;
+globalargs args={0, 0, 0, 0, 0, NULL, 0};
+printflags flags={0,0,0,0,0};
 const char *optstring="nvETs";
 char src[256];
-int squeezeflag=0;
+char *name;
+int squeezecount=0;
+int squeezeflag1=0;
+int squeezeflag2=0;
 int numberflag=1;
+int firstrowflag=1;
+int i=1;//file
+int j=1;//stdin
 void readfile(char* filename);
-void print(unsigned char c,int *i);
-//int checkstdi(char *arg);
+void print(unsigned char c);
 int main(int argc, char *argv[]) {
+    name=argv[0];
     int opt=0;
     if(argc>1) {
         opt = getopt(argc, argv, optstring);
@@ -42,7 +56,8 @@ int main(int argc, char *argv[]) {
                 case 's':
                     args.squeezeblank = 1;
                     break;
-                default:
+                case '?':
+                    exit(1);
                     break;
             }
             opt = getopt(argc, argv, optstring);
@@ -50,115 +65,146 @@ int main(int argc, char *argv[]) {
         args.files = argv + optind;
         args.numfiles = argc - optind;
         for (int i = 0; i < args.numfiles; i++) {
+            squeezecount=0;
+            squeezeflag1=0;
+            numberflag=1;
             readfile(args.files[i]);
         }
     } else
         readfile("-");
-   /* if(argc>1) {
-        for (int i = 1; i < argc; i++) {//ПЕРЕБОР ПАРАМЕТРОВ
-            if(i==1&&argc!=2) {
-                if (strlen(argv[i]) == 2) {//ВОЗМОЖНО, ЗДЕСЬ КЛЮЧИ
-                    if (strcmp(argv[i],"-E")==0)
-                        showends = 1;
-                    else if (strcmp(argv[i],"-n")==0)
-                        number = 1;
-                    else if (strcmp(argv[i],"-s")==0)
-                        squeezeblank = 1;
-                    else if (strcmp(argv[i],"-T")==0)
-                        showtabs = 1;
-                    else if (strcmp(argv[i],"-v")==0)
-                        shownonprinting = 1;
-                    else
-                        readfile(argv[i]);
-                }
-                else {
-                    readfile(argv[i]);
-                }
-            }
-            else{
-                readfile(argv[i]);
-            }
-        }
-    }
-    else{
-        readfile("-");
-    }*/
     return 0;
 }
 
-void printnonprinting(unsigned char c){
+int printnonprinting(unsigned char c){
     if(c>=0&&c<=37&&c!=9&&c!=10&&c!=12) {
         c = c+64;
         write(STDOUT_FILENO,"^",1);
         write(STDOUT_FILENO,&c,1);
+        return 1;
     }
-    else if(c==177)
-        write(STDOUT_FILENO,"^?",2);
-    else
-        write(STDOUT_FILENO,&c,1);
+    else if(c==177) {
+        write(STDOUT_FILENO, "^?", 2);
+        return 1;
+    }
+    return 0;
 }
-void printwithtabs(unsigned char c){
-    if(c==9)
-        write(STDOUT_FILENO,"^I",2);
-    else
-        write(STDOUT_FILENO,&c,1);
+int printwithtabs(unsigned char c){
+    if(c==9) {
+        write(STDOUT_FILENO, "^I", 2);
+        return 1;
+    }
+    return 0;
 }
-void printwithends(unsigned char c){
-    if(c==10)
-        write(STDOUT_FILENO,"$\n",2);
-    else
-        write(STDOUT_FILENO,&c,1);
+int printwithends(unsigned char c){
+    if(c==10) {
+        if(!squeezeflag1) {
+            write(STDOUT_FILENO, "$", 1);
+            write(STDOUT_FILENO, &c, 1);
+        }
+        squeezeflag1=0;
+        return 1;
+    }
+    squeezeflag1=0;
+    return 0;
 }
-void printwithnumberfile(unsigned char c, int *i){
+int printwithnumberfile(unsigned char c){
+    if(firstrowflag){
+        printf("%6d  ", i);
+        i++;
+        firstrowflag=0;
+    }
     if (c == 10) {
-        printf("\n%6d  ", *i);
-        (*i)++;
-    } else
-        write(STDOUT_FILENO,&c,1);
+        if(!squeezeflag2) {
+            if (!flags.endsflag)
+                write(STDOUT_FILENO, &c, 1);
+            printf("%6d  ", i);
+            i++;
+        }
+        squeezeflag2=0;
+        return 1;
+    }
+    return 0;
 }
-void printwithnumberstdin(unsigned char c,int *i){
+int printwithnumberstdin(unsigned char c){
     if(numberflag){
         numberflag=0;
-        char buf[10];
-        printf("%6d  ", *i);
+        printf("%6d  ", i);
         write(STDOUT_FILENO,&c,1);
-        (*i)++;
-    } else
-        write(STDOUT_FILENO,&c,1);
+        i++;
+        return 1;
+    }
+    return 0;
 }
-void printsqueezeblank(unsigned char c){
-    if(squeezeflag > 1) {
+int printsqueezeblank(unsigned char c){
+    if(squeezecount > 1) {
         if (c != 10){
             write(STDOUT_FILENO,&c,1);
-            squeezeflag=0;
+            squeezecount=0;
         }
+        else {
+            squeezecount++;
+            squeezeflag1=1;
+            squeezeflag2=1;
+        }
+        return 1;
     }
     else{
         if(c==10)
-            squeezeflag++;
+            squeezecount++;
         else
-            squeezeflag=0;
-        write(STDOUT_FILENO,&c,1);
+            squeezecount=0;
+        //write(STDOUT_FILENO,&c,1);
+        return 0;
     }
 }
-void print(unsigned char c, int *i){
+void resetflags(){
+    flags.nonprintingflag=0;
+    flags.tabsflag=0;
+    flags.endsflag=0;
+    flags.numberflag=0;
+    flags.squeezeblankflag=0;
+}
+void print(unsigned char c){//порядок if-ов важен!
+    if(args.squeezeblank)
+        flags.squeezeblankflag=printsqueezeblank(c);
     if(args.shownonprinting)
+        flags.nonprintingflag=printnonprinting(c);
+    if(args.showtabs)
+        flags.tabsflag=printwithtabs(c);
+    if(args.showends)
+        flags.endsflag=printwithends(c);
+    if(args.number)
+        flags.numberflag=printwithnumberfile(c);
+    if(!flags.nonprintingflag&&!flags.tabsflag&&!flags.endsflag&&!flags.numberflag&&!flags.squeezeblankflag)
+        write(STDOUT_FILENO,&c,1);
+    resetflags();
+}
+void printstdin(unsigned char c){
+    if (args.shownonprinting)
         printnonprinting(c);
-    else if(args.showtabs)
+    else if (args.showtabs)
         printwithtabs(c);
-    else if(args.showends)
+    else if (args.showends)
         printwithends(c);
-    else if(args.number)
-        printwithnumberfile(c, i);
-    else if(args.squeezeblank)
+    else if (args.number) {
+        printwithnumberstdin(c);
+        if(c==10){
+            numberflag=1;
+        }
+    }
+    else if (args.squeezeblank)
         printsqueezeblank(c);
     else
         write(STDOUT_FILENO,&c,1);
 }
-
+void file_err(char* filename){
+    strcat(src,name);
+    strcat(src,": ");
+    strcat(src, filename);
+    perror(src);
+    exit(1);
+}
 void readfile(char* filename){
-    int i=2;
-    int j=1;
     int handle;
     unsigned char ch;
     if(strcmp(filename,"-")==0){//stdin
@@ -170,41 +216,23 @@ void readfile(char* filename){
             if (n == 0) {
                 break;
             } else {
-                if (args.shownonprinting)
-                    printnonprinting(c);
-                else if (args.showtabs)
-                    printwithtabs(c);
-                else if (args.showends)
-                    printwithends(c);
-                else if (args.number) {
-                    printwithnumberstdin(c, &j);
-                    if(c==10){
-                        numberflag=1;
-                    }
-                }
-                else if (args.squeezeblank)
-                    printsqueezeblank(c);
-                else
-                    write(STDOUT_FILENO,&c,1);
+                printstdin(c);
             }
         }
     }
     else {//file
         if ((handle = open(filename, O_RDONLY)) == -1) {
-            strcat(src, "mycat: ");
-            strcat(src, filename);
-            perror(src);
-            exit(1);
+            file_err(filename);
         }
-        if (args.number)
-            write(STDOUT_FILENO,"\n     1  ",9);
+        /*if (args.number&&firstrowflag) {
+            write(STDOUT_FILENO, "     1  ", 9);
+            firstrowflag=0;
+        }*/
         while (!eof(handle)) {
             if (read(handle, &ch, 1) == -1) {
-                strcat(src, "mycat: ");
-                strcat(src, filename);
-                perror(src);
+                file_err(filename);
             }
-            print(ch, &i);
+            print(ch);
         }
         close(handle);
     }
