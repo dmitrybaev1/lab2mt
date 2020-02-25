b1255d2559cbeceea96d87a4eb525c6ee0503b59
dmitrybaev1 <223868@niuitmo.ru>
2020-02-26 00:40:57 +0300

    final

--- lab2Multithreading/main.c
+++ lab2Multithreading/main.c
@@ -16,13 +16,14 @@ typedef struct globalArgs_t{
 }globalargs;
 globalargs args={0, 0, 0, 0, 0, NULL, 0};
 const char *optstring="nvETs";
-int number=0;
+int numberofrow=0;
 int neednumber=1;
 int emptyrow=0;
 char src[256];
 char *catname;
 void readfromfile(char* filename);
-void print(char* s, int bytes);
+void print(char* s, int bytes,char* filename);
+void file_err(char* filename);
 
 int main(int argc, char *argv[]) {
     catname=argv[0];
@@ -65,14 +66,14 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void printrow(char* row,int count){
+void printrow(char* row,int count,char* filename){
     int tabsflag=0;
     int endsflag=0;
     int nonprintingflag=0;
     if(args.number){
         if(neednumber) {
-            number++;
-            printf("%6d  ", number);
+            numberofrow++;
+            printf("%6d  ", numberofrow);
         }
         else
             neednumber=1;
@@ -80,30 +81,36 @@ void printrow(char* row,int count){
     for(int i=0;i<count;i++){
         if(args.showtabs)
             if(row[i]==9) {
-                write(STDOUT_FILENO, "^I", 2);
+                if(write(STDOUT_FILENO, "^I", 2)==-1)
+                    file_err(filename);
                 tabsflag=1;
             }
         if(args.shownonprinting){
             unsigned char c = row[i];
-            if(c>=0&&c<=37&&c!=9&&c!=10&&c!=12) {
+            if(c<=37&&c!=9&&c!=10&&c!=12) {
                 c = c + 64;
-                write(STDOUT_FILENO, "^", 1);
-                write(STDOUT_FILENO, &c, 1);
+                if(write(STDOUT_FILENO, "^", 1)==-1)
+                    file_err(filename);
+                if(write(STDOUT_FILENO, &c, 1)==-1)
+                    file_err(filename);
                 nonprintingflag=1;
             }
             else if(c==177) {
-                write(STDOUT_FILENO, "^?", 2);
+                if(write(STDOUT_FILENO, "^?", 2)==-1)
+                    file_err(filename);
                 nonprintingflag=1;
             }
         }
         if(args.showends){
             if(row[i]==10){
-                write(STDOUT_FILENO, "$\n", 2);
+                if(write(STDOUT_FILENO, "$\n", 2)==-1)
+                    file_err(filename);
                 endsflag=1;
             }
         }
         if(!endsflag&&!tabsflag&&!nonprintingflag)
-            write(STDOUT_FILENO,&row[i],1);
+            if(write(STDOUT_FILENO,&row[i],1)==-1)
+                file_err(filename);
         tabsflag=0;
         endsflag=0;
         nonprintingflag=0;
@@ -142,17 +149,17 @@ char* getrow(int* i,int* count,char* s,int bytes){
     *count=k-1;
     return row;
 }
-void print(char* s, int bytes){
+void print(char* s, int bytes, char* filename){
    int i=0;
    int count;
    while (i<bytes) {
        char* row = getrow(&i,&count,s,bytes);
        if(args.squeezeblank){
            if(emptyrow<=1)
-               printrow(row, count);
+               printrow(row, count,filename);
        }
        else
-           printrow(row, count);
+           printrow(row, count,filename);
        free(row);
    }
 }
@@ -172,7 +179,7 @@ void readfromfile(char* filename){
             file_err(filename);
         }
         while ((bytes = read(handle, buf, BUF_SIZE)) > 0) {
-            print(buf, bytes);
+            print(buf, bytes,filename);
         }
         if (bytes == -1)
             file_err(filename);
@@ -180,7 +187,7 @@ void readfromfile(char* filename){
     }
     else{//stdin
         while ((bytes = read(STDIN_FILENO, buf, BUF_SIZE)) > 0) {
-            print(buf, bytes);
+            print(buf, bytes,filename);
         }
         if (bytes == -1)
             file_err(filename);
