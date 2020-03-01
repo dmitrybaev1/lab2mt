a95693c98e376bbba5efa20fbf1e1a59905a4b72
dmitrybaev1 <223868@niuitmo.ru>
2020-03-01 14:49:42 +0300

    для винды

--- lab2Multithreading/main.c
+++ lab2Multithreading/main.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-#include <io.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
+#include <string.h>
 #define BUF_SIZE 4096
 typedef struct globalArgs_t{
     int number;// -n
-    int shownonprinting;// -v
     int showends;// -E
     int showtabs;// -T
     int squeezeblank;// -s
     char **files;
     int numfiles;
 }globalargs;
-globalargs args={0, 0, 0, 0, 0, NULL, 0};
-const char *optstring="nvETs";
+globalargs args={0, 0, 0, 0, NULL, 0};
+const char *optstring="nETs";
 int numberofrow=0;
 int neednumber=1;
 int emptyrow=0;
@@ -34,9 +33,6 @@ int main(int argc, char *argv[]) {
             switch (opt) {
                 case 'n':
                     args.number = 1;
-                    break;
-                case 'v':
-                    args.shownonprinting = 1;
                     break;
                 case 'E':
                     args.showends = 1;
@@ -69,7 +65,6 @@ int main(int argc, char *argv[]) {
 void printrow(char* row,int count,char* filename){
     int tabsflag=0;
     int endsflag=0;
-    int nonprintingflag=0;
     if(args.number){
         if(neednumber) {
             numberofrow++;
@@ -79,41 +74,25 @@ void printrow(char* row,int count,char* filename){
             neednumber=1;
     }
     for(int i=0;i<count;i++){
+        unsigned char c = row[i];
         if(args.showtabs)
-            if(row[i]==9) {
+            if(c==9) {
                 if(write(STDOUT_FILENO, "^I", 2)==-1)
                     file_err(filename);
                 tabsflag=1;
             }
-        if(args.shownonprinting){
-            unsigned char c = row[i];
-            if(c<=37&&c!=9&&c!=10&&c!=12) {
-                c = c + 64;
-                if(write(STDOUT_FILENO, "^", 1)==-1)
-                    file_err(filename);
-                if(write(STDOUT_FILENO, &c, 1)==-1)
-                    file_err(filename);
-                nonprintingflag=1;
-            }
-            else if(c==177) {
-                if(write(STDOUT_FILENO, "^?", 2)==-1)
-                    file_err(filename);
-                nonprintingflag=1;
-            }
-        }
         if(args.showends){
-            if(row[i]==10){
+            if(c==10){
                 if(write(STDOUT_FILENO, "$\n", 2)==-1)
                     file_err(filename);
                 endsflag=1;
             }
         }
-        if(!endsflag&&!tabsflag&&!nonprintingflag)
-            if(write(STDOUT_FILENO,&row[i],1)==-1)
+        if(!endsflag&&!tabsflag)
+            if(write(STDOUT_FILENO,&c,1)==-1)
                 file_err(filename);
         tabsflag=0;
         endsflag=0;
-        nonprintingflag=0;
     }
     if(row[count-1]!=10)//если последний символ строки не перенос
         neednumber=0;
@@ -175,7 +154,7 @@ void readfromfile(char* filename){
     char buf[BUF_SIZE];
     int bytes;
     if(strcmp(filename,"-")!=0) {//file
-        if ((handle = open(filename, O_RDONLY)) == -1) {
+        if ((handle = open(filename, O_RDONLY|O_BINARY)) == -1) {
             file_err(filename);
         }
         while ((bytes = read(handle, buf, BUF_SIZE)) > 0) {
